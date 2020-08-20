b5b698ce91b039afeff341f824708ec16965f56e
Jonah Henriksson <33059163+JonahHenriksson@users.noreply.github.com>
2020-08-20 14:12:46 -0400

    1.2 - Added support for creating new files Added a context menu option for creating new RON files. Makes it easier than having to create a file and type the extension.

---
+++ src/main/java/org/jonahhenriksson/ron/language/RONCreateFile.java
@@ -0,0 +1,30 @@
+package org.jonahhenriksson.ron.language;
+
+import com.intellij.ide.actions.CreateFileFromTemplateAction;
+import com.intellij.ide.actions.CreateFileFromTemplateDialog;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.NlsActions;
+import com.intellij.openapi.util.NlsContexts;
+import com.intellij.psi.PsiDirectory;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+public class RONCreateFile extends CreateFileFromTemplateAction {
+
+    private static final String NAME = "RON File";
+
+    public RONCreateFile() {
+        super(NAME, "Create new RON file", RONIcons.FILE);
+    }
+
+    @Override
+    protected void buildDialog(@NotNull Project project, @NotNull PsiDirectory directory, CreateFileFromTemplateDialog.@NotNull Builder builder) {
+        builder.setTitle(NAME).addKind("Empty file", RONIcons.FILE, "RON File");
+    }
+
+    @Override
+    protected @NlsContexts.Command String getActionName(PsiDirectory directory, @NotNull String newName, String templateName) {
+        return NAME;
+    }
+}
